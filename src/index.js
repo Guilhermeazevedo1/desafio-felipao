4a7f1db506edfcf4c3ea49ff82d8cca484987883
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2024-11-08 20:42:26 -0300

    Entrega do desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("oi")
+let xp = 9001;
+let heroi = "k0vu";
+
+if (xp <= 1000) {
+    console.log(`O herói de nome ${heroi} está no nível Ferro`);
+} else if (xp >= 1001 && xp <= 2000) {
+    console.log(`O herói de nome ${heroi} está no nível Bronze`);
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log(`O herói de nome ${heroi} está no nível Prata`);
+} else if (xp >= 5001 && xp <= 7000) {
+    console.log(`O herói de nome ${heroi} está no nível Ouro`);
+} else if (xp >= 7001 && xp <= 8000) {
+    console.log(`O herói de nome ${heroi} está no nível Platina`);
+} else if (xp >= 8001 && xp <= 9000) {
+    console.log(`O herói de nome ${heroi} está no nível Ascendente`);
+} else if (xp >= 9001 && xp <= 10000) {
+    console.log(`O herói de nome ${heroi} está no nível Imortal`);
+} else {
+    console.log(`O herói de nome ${heroi} está no nível Radiante`);
+}
+
